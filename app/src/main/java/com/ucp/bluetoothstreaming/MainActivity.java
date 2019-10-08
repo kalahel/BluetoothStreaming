192ee000219a9403cf7738cc44b293f2b09e320c
Mathieu Hannoun <mathieu95480@gmail.com>
2019-10-08 14:48:32 +0200

    :sparkles: Added request for permissions

--- app/src/main/java/com/ucp/bluetoothstreaming/MainActivity.java
+++ app/src/main/java/com/ucp/bluetoothstreaming/MainActivity.java
@@ -1,9 +1,14 @@
 package com.ucp.bluetoothstreaming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+        else {
+            // Permission has already been granted
+            Log.d("MAIN_ACTIVITY", "WRITE PERMISSION ALREADY GRANTED");
+        }
+
     }
 
     public void goToServerActivity(View view) {
