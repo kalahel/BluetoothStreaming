0722291f785494cdd57dfc51424bebc3bf2916ad
sebastien <barre.sebastien.22@gmail.com>
2019-10-09 13:21:02 +0200

    :sparkles: bluetooth connexion client-server works !

--- app/src/main/java/com/ucp/bluetoothstreaming/ClientServerPairing.java
+++ app/src/main/java/com/ucp/bluetoothstreaming/ClientServerPairing.java
@@ -2,7 +2,13 @@ package com.ucp.bluetoothstreaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ucp.bluetoothstreaming.Services.BluetoothClientService;
+import com.ucp.bluetoothstreaming.Services.BluetoothServerService;
 
 public class ClientServerPairing extends AppCompatActivity {
 
@@ -10,6 +16,20 @@ public class ClientServerPairing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_server_pairing);
+
+
+        BluetoothDevice bd = getIntent().getExtras().getParcelable(ClientActivity.INTENT_SELECTOR);
+        if (bd == null) {
+            Log.e("bluetooth device transmission", "FAIL");
+        } else {
+            Log.e("bluetooth device transmission", "OK");
+
+        }
+        Intent startBluetoothCLientIntent = new Intent(this, BluetoothClientService.class);
+
+        startBluetoothCLientIntent.putExtra(BluetoothClientService.TAG_INTENT, bd);
+        startService(startBluetoothCLientIntent);
+
 
     }
 }
---
+++ app/src/main/java/com/ucp/bluetoothstreaming/Services/BluetoothClientService.java
@@ -0,0 +1,127 @@
+package com.ucp.bluetoothstreaming.Services;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.ucp.bluetoothstreaming.ClientActivity;
+
+import java.io.IOException;
+
+public class BluetoothClientService extends Service {
+
+
+    public static final String TAG = "BLUETOOTH_CLIENT_SERVICE";
+    public static final String TAG_INTENT = "BLUETOOTH_CLIENT_INTENT";
+
+    private final IBinder mBinder = new BluetoothClientService.LocalBinder();  // interface for clients that bind
+    private Thread clientThread ;
+
+
+
+    public BluetoothClientService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        BluetoothDevice bd = intent.getExtras().getParcelable(TAG_INTENT);
+        clientThread = new ConnectThread(bd);
+        clientThread.start();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     * Source : https://developer.android.com/guide/components/bound-services.html#Binder
+     */
+    public class LocalBinder extends Binder {
+        public BluetoothClientService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return BluetoothClientService.this;
+        }
+    }
+
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(BluetoothServerService.APP_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            bluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            manageMyConnectedSocket(mmSocket);
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+
+        private void manageMyConnectedSocket(BluetoothSocket socket) {
+            // TODO FIll
+            Log.d(TAG,"Connected to the server !");
+        }
+    }
+
+
+}
